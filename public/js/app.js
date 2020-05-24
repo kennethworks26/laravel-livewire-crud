38a4d2bcd1efb3c45e1d5359b96a9ee1f4c9bf93
Kenneth Fernandez <kenneth@mesasix.com>
2020-05-24 14:45:24 +0800

    Add middleware auth in Employee and Company Controllers

--- public/js/app.js
+++ public/js/app.js
@@ -37292,8 +37292,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/mesasix/Sites/personal-junior-laravel/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/mesasix/Sites/personal-junior-laravel/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/kenneth/Laravel/personal-junior-developer/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/kenneth/Laravel/personal-junior-developer/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
