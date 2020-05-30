5c4a1b178c8ec2662387ee88aa41d48ea364e14c
Kenneth Fernandez <kenneth@mesasix.com>
2020-05-30 21:51:37 +0800

    Update logo field to input[type=file] in companies.create view

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -8,55 +8,48 @@
                 <div class="card-header">Create Company</div>
                 <div class="card-body">
 
-                @if ($errors->any())
+                    @if ($errors->any())
                     <div class="alert alert-danger">
                         <ul>
                             @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
+                            <li>{{ $error }}</li>
                             @endforeach
                         </ul>
                     </div>
-                @endif
+                    @endif
 
-                <form method="post" action="{{ route('companies.store')}}">
-                    @csrf
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name" name="name" placeholder="">
-                        @error('name')
+                    <form method="post" action="{{ route('companies.store')}}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="">
+                            @error('name')
                             <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" placeholder="">
-                        @error('email')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">Upload</span>
-                            </div>
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="logo" name="logo">
-                                <label class="custom-file-label" for="logo">Choose file</label>
-                            </div>
+                            @enderror
                         </div>
-                        @error('logo')
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="">
+                            @error('email')
                             <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="website">Website</label>
-                        <input type="url" class="form-control" id="website" name="website" placeholder="">
-                        @error('website')
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="logo">Logo</label>
+                            <input type="file" class="form-control-file" id="logo" name="logo">
+                            @error('logo')
                             <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="website">Website</label>
+                            <input type="url" class="form-control" id="website" name="website" placeholder="">
+                            @error('website')
+                            <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
                 </div>
             </div>
         </div>
