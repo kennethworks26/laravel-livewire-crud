6fdc615ede55794381e98bc20fcedda65eb86526
Kenneth Fernandez <kenneth@mesasix.com>
2020-06-08 22:45:25 +0800

    Update companies.index to use livewire

--- app/Http/Livewire/CompaniesTable.php
+++ app/Http/Livewire/CompaniesTable.php
@@ -3,11 +3,35 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Company;
 
 class CompaniesTable extends Component
 {
+    use WithPagination;
+
+    public $perPage = 10;
+    public $sortField = 'name';
+    public $sortAsc = true;
+    public $search = '';
+
+    public function sortBy($field)
+    {
+        if ($this->sortField === $field) {
+            $this->sortAsc = !$this->sortAsc;
+        } else {
+            $this->sortAsc = true;
+        }
+
+        $this->sortField = $field;
+    }
+
     public function render()
     {
-        return view('livewire.companies-table');
+        return view('livewire.companies-table', [
+            'companies' => \App\Company::search($this->search)
+                ->orderBy($this->sortField, $this->sortAsc ? 'asc' : 'desc')
+                ->paginate($this->perPage),
+        ]);
     }
 }
--- resources/views/livewire/companies-table.blade.php
+++ resources/views/livewire/companies-table.blade.php
@@ -1,3 +1,58 @@
 <div>
-    {{-- Be like water. --}}
+    <div class="row mb-4">
+        <div class="col-md-8 form-inline">
+            Per Page: &nbsp;
+            <select wire:model="perPage" class="form-control">
+                <option>10</option>
+                <option>15</option>
+                <option>25</option>
+            </select>
+        </div>
+
+        <div class="col-md-4">
+            <input wire:model="search" class="form-control" type="text" placeholder="Search...">
+        </div>
+    </div>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th wire:click.prevent="sortBy('name')" role="button" scope="col">Name</th>
+                <th wire:click.prevent="sortBy('email')" role="button" scope="col">Email</th>
+                <th wire:click.prevent="sortBy('website')" role="button" scope="col">Website</th>
+                <th role="button" scope="col"># of Employees</th>
+                <th scope="col" width="150">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($companies as $company)
+            <tr>
+                <td><a href="{{ route('companies.show', $company->id) }}">{{ $company->name }}</a></td>
+                <td>{{ $company->email }}</td>
+                <td>{{ $company->website }}</td>
+                <td>{{ $company->employees_count }}</td>
+                <td>
+                    <div class="d-flex">
+                        <a class="btn btn-primary mr-1" href="{{ route('companies.edit', $company->id) }}">Edit</a>
+                        <form action="{{ route('companies.destroy', $company->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <div class="row">
+        <div class="col">
+            {{ $companies->links() }}
+        </div>
+
+        <div class="col text-right text-muted">
+            Showing {{ $companies->firstItem() }} to {{ $companies->lastItem() }} out of {{ $companies->total() }} results
+        </div>
+    </div>
 </div>
